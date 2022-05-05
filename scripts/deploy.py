33ef6f28d8d33a88ffe1aa2161940975d7523f6a
Maty Hattam <maty.hattam@gmail.com>
2022-05-05 22:04:50 +0200

    initial commit

---
+++ scripts/deploy.py
@@ -0,0 +1,9 @@
+from brownie import accounts, config
+import os
+
+def deploy_simple_storage():
+    account = accounts.add(config["wallets"]["from_key"])
+    print(account)
+
+def main():
+    deploy_simple_storage()
